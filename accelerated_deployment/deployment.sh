00855d8a126154df3b359dac9cc3763d4bedb17d
Benjamin Arntzen <zorlin@gmail.com>
2024-03-04 13:44:34 +0000

    Updated deployment script

--- accelerated_deployment/deployment.sh
+++ accelerated_deployment/deployment.sh
@@ -3,4 +3,6 @@ kubectl rollout status --watch --timeout=30000s statefulset/bootstrap -n zerotes
 kubectl apply -f midstrap.yaml
 kubectl rollout status --watch --timeout=30000s statefulset/midstrap -n zerotesting
 kubectl apply -f nodes.yaml
-kubectl rollout status --watch --timeout=30000s statefulset/nodes -n zerotesting
+echo "We have deployed all nodes, please watch Prometheus or Grafana to see when they have reached a healthy state."
+echo "Please note you cannot (yet) rely on the Ready state as it does not actually indicate an unhealthy peer, just one that is not ready for bootstrapping from."
+#kubectl rollout status --watch --timeout=30000s statefulset/nodes -n zerotesting

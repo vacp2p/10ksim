9afc94b28a40cc485ae8248db15094ee6418c80e
Alberto Soutullo <alberto.soutullo639@gmail.com>
2024-03-13 18:31:18 +0100

    Fixed logger to avoid logging external libraries

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -1,8 +1,7 @@
 import logging.config
 import yaml
 
+
 with open('src/logger/logger_config.yaml', 'r') as f:
     config = yaml.safe_load(f.read())
     logging.config.dictConfig(config)
-
-log = logging.getLogger(__name__)

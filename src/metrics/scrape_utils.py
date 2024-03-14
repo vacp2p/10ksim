d75eb1397edf8919663e1bff7a25cc82c02a2d48
Alberto Soutullo <alberto.soutullo639@gmail.com>
2024-03-14 15:00:36 +0100

    Changed scrapper to select initial and final time

--- src/metrics/scrape_utils.py
+++ src/metrics/scrape_utils.py
@@ -1,22 +1,21 @@
 # Pyton Imports
-import datetime
+from datetime import datetime
 
 
 # Having now in an external function allows us to mock it in the tests
-def _get_datetime_now() -> datetime.datetime:
-    return datetime.datetime.now()
+def _get_datetime_now() -> datetime:
+    return datetime.now()
 
 
-def create_promql(address: str, query: str, hours_passed: int, step: int) -> str:
+def create_promql(address: str, query: str, start_scrape: str, finish_scrape: str, step: int) -> str:
     promql = address + "query_range?query=" + query
 
-    start = datetime.datetime.timestamp(
-        _get_datetime_now() - datetime.timedelta(hours=hours_passed))
-    now = datetime.datetime.timestamp(_get_datetime_now())
+    start = datetime.strptime(start_scrape, "%Y-%m-%d %H:%M:%S").timestamp()
+    end = datetime.strptime(finish_scrape, "%Y-%m-%d %H:%M:%S").timestamp()
 
     promql = (promql +
               "&start=" + str(start) +
-              "&end=" + str(now) +
+              "&end=" + str(end) +
               "&step=" + str(step))
 
     return promql

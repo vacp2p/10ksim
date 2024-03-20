b766db46c61d4bfa25aee9c5370d019b4be3bfb2
Alberto Soutullo <alberto.soutullo639@gmail.com>
2024-03-20 17:31:59 +0100

    Extract order function to list utils file

---
+++ src/utils/list.py
@@ -0,0 +1,24 @@
+# Python Import
+from itertools import chain
+from typing import List
+
+
+def order_by_groups(list_to_order: List) -> List:
+    # TODO: change this to a more generic function
+    def get_default_format_id(val):
+        return int(val.split('-')[1].split('_')[0])
+
+    nodes = []
+    bootstrap = []
+    others = []
+    for item in list_to_order:
+        if item.startswith('nodes'):
+            nodes.append(item)
+        elif item.startswith('bootstrap'):
+            bootstrap.append(item)
+        else:
+            others.append(item)
+    nodes.sort(key=get_default_format_id)
+    bootstrap.sort(key=get_default_format_id)
+
+    return list(chain(others, bootstrap, nodes))

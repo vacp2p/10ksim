7230916e68a58e1c0b053fc55336e62a72f8c03c
Alberto Soutullo <alberto.soutullo639@gmail.com>
2024-03-22 18:17:11 +0100

    Modified data file handler

--- src/data/data_file_handler.py
+++ src/data/data_file_handler.py
@@ -1,6 +1,8 @@
 # Python Imports
 import logging
 import pandas as pd
+from pathlib import Path
+from typing import List
 from result import Result, Ok, Err
 
 # Project Imports
@@ -15,27 +17,38 @@ class DataFileHandler(DataHandler):
         super().__init__()
         self._dataframe = pd.DataFrame()
 
-    def add_dataframes_from_folder_as_mean(self, folder_path: str) -> Result[str, str]:
-        data_files_path = file_utils.get_files_from_folder_path(folder_path)
+    def add_dataframes_from_folders_as_mean(self, folder_paths: List) -> Result[str, str]:
+        for folder in folder_paths:
+            folder = Path(folder)
+            match file_utils.get_files_from_folder_path(folder):
+                case Ok(data_files_names):
+                    self._add_files_as_mean(data_files_names, folder)
+                    # TODO: set class with yaml upgrades
+                    self._dataframe["class"] = str(folder.parents[0]) + folder.name
+                    msg = f"{folder_paths} added."
+                    return Ok(msg)
+                case Err(error):
+                    return Err(error)
 
+    def _add_files_as_mean(self, data_files_path: List, location: Path):
         for file_path in data_files_path:
-            self.add_dataframe_from_file_as_mean(file_path)
+            match self.add_dataframe_from_file_as_mean(location / file_path):
+                case Ok(msg):
+                    logger.info(msg)
+                case Err(msg):
+                    logger.error(msg)
 
-        # TODO: set class with yaml upgrades
-        self._dataframe["class"] = folder_path.split("/")[-2]
+    def add_dataframe_from_file_as_mean(self, file_path: Path) -> Result[str, str]:
+        if file_utils.check_if_path_exists(file_path):
+            self._dump_mean_df(file_path)
+            return Ok(f"{file_path} dumped to memory.")
 
-    def add_dataframe_from_file_as_mean(self, file_path: str) -> Result[str, str]:
-        match file_utils.check_if_file_exists(file_path):
-            case Ok(_):
-                self._dump_mean_df(file_path)
-                return Ok("")
-            case Err(_):
-                return Err("")
+        return Err(f"{file_path} cannot be dumped to memory.")
 
     def get_dataframe(self) -> pd.DataFrame:
         return self._dataframe
 
-    def _dump_mean_df(self, file_path: str):
+    def _dump_mean_df(self, file_path: Path):
         df = pd.read_csv(file_path, parse_dates=['Time'], index_col='Time')
         file_name = file_utils.get_file_name_from_path(file_path)
         df_mean = df.mean()

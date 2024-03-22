0b81c0a6e02bc7207b186ba91f98b5c10689b427
Alberto Soutullo <alberto.soutullo639@gmail.com>
2024-03-22 18:16:54 +0100

    Created Parent class for data handler

--- src/data/data_handler.py
+++ src/data/data_handler.py
@@ -5,29 +5,26 @@ from typing import List
 
 # Project Imports
 from src.utils import path
-from src.utils import list
 
 logger = logging.getLogger(__name__)
 
 
 class DataHandler:
-    def __init__(self, data):
-        self._raw_data = data
-        self._data_df = pd.DataFrame()
+    def __init__(self):
+        self._dataframe = None
 
-    def create_dataframe_from_request(self, placeholder):
-        data_result = self._raw_data['data']['result']
+    @staticmethod
+    def concatenate_dataframes(dataframes: List, vertically: bool = True) -> pd.DataFrame:
+        if vertically:
+            return pd.concat(dataframes)
 
-        for pod_result_dict in data_result:
-            final_column_name = self._extract_column_name_from_result(placeholder, pod_result_dict)
-            values = pod_result_dict['values']
+        return pd.concat(dataframes, axis="columns")
 
-            pod_df = self._create_pod_df(final_column_name, values)
+    @staticmethod
+    def prepare_dataframe_for_boxplot(dataframe: pd.DataFrame, class_name='class') -> pd.DataFrame:
+        prepared_df = pd.melt(dataframe, class_name)
 
-            self._data_df = pd.merge(self._data_df, pod_df, how='outer', left_index=True,
-                                     right_index=True)
-
-        self._sort_dataframe_columns()
+        return prepared_df
 
     def dump_dataframe(self, out_folder: str, file_name: str):
         result = path.prepare_path(out_folder, file_name)
@@ -35,23 +32,5 @@ class DataHandler:
             logger.error(f'{result.err_value}')
             exit(1)
 
-        self._data_df.to_csv(result.ok_value)
+        self._dataframe.to_csv(result.ok_value)
         logger.info(f'{file_name} data dumped')
-
-    def _extract_column_name_from_result(self, placeholder, pod_result_dict):
-        placeholder_items = placeholder.split('-')
-        metric_info = pod_result_dict['metric']
-        final_column_name = '_'.join(metric_info[key] for key in placeholder_items)
-
-        return final_column_name
-
-    def _create_pod_df(self, column_name: str, values: List) -> pd.DataFrame:
-        pod_df = pd.DataFrame(values, columns=['Time', column_name])
-        pod_df['Time'] = pd.to_datetime(pod_df['Time'], unit='s')
-        pod_df.set_index('Time', inplace=True)
-
-        return pod_df
-
-    def _sort_dataframe_columns(self):
-        columns = list.order_by_groups(self._data_df.columns.tolist())
-        self._data_df = self._data_df[columns]

406753c04b0da5052506f2d35bccefaddf9c68aa
Alberto Soutullo <alberto.soutullo639@gmail.com>
2024-03-22 13:31:44 +0100

    Add function to file_utils.py

--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -1,10 +1,15 @@
 # Python Imports
 import os
 import yaml
+import logging
 from pathlib import Path
 from typing import List, Dict
+from result import Result, Err, Ok
 
 # Project Imports
+
+
+logger = logging.getLogger(__name__)
 
 
 def read_yaml_file(file_path: str) -> Dict:
@@ -23,3 +28,10 @@ def get_files_from_folder_path(path: str) -> List:
 def get_file_name_from_path(file_path: str) -> str:
     return file_path.split("/")[-1]
 
+
+def check_if_file_exists(file_path: str) -> Result[bool, bool]:
+    if not os.path.exists(file_path):
+        logger.error(f"{file_path} does not exists.")
+        return Err(False)
+
+    return Ok(True)

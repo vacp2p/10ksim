c72f9e069d8d17943bbbb42c5f1f0074226a384a
Alberto Soutullo <alberto.soutullo639@gmail.com>
2024-03-27 16:33:27 +0100

    Use Path.parent, and remove wrongly added methods

--- src/utils/path.py
+++ src/utils/path.py
@@ -11,36 +11,23 @@ logger = logging.getLogger(__name__)
 
 def prepare_path(file_location: str) -> Result[Path, str]:
     file_location_path = Path(file_location)
-    dirname = get_parent_folders_from_file_path(file_location_path)
 
     if file_location_path.exists():
         logger.warning(f'{file_location_path} is already existing. File will be overwritten.')
 
     try:
-        Path(dirname).mkdir(parents=True, exist_ok=True)
+        file_location_path.parent.mkdir(parents=True, exist_ok=True)
     except OSError as e:
-        return Err(f'Error creating {dirname}. {e}')
+        return Err(f'Error creating {file_location_path.parent}. {e}')
 
     return Ok(file_location_path)
 
 
 def get_files_from_folder_path(path: Path) -> Result[List, str]:
-    if check_if_path_exists(path):
+    if path.exists():
         files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
         logger.info(f"Found {len(files)} in {path}")
         logger.debug(f"Files are: {files}")
         return Ok(files)
 
     return Err(f"{path} does not exist.")
-
-
-def get_file_name_from_path(file_path: Path) -> str:
-    return file_path.name
-
-
-def get_parent_folders_from_file_path(file_path: Path) -> str:
-    return os.path.dirname(file_path)
-
-
-def check_if_path_exists(path: Path) -> bool:
-    return os.path.exists(path)

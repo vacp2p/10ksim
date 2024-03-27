9bc2ec3d56ffa105d1f5a40f585e40529af2fc16
Alberto Soutullo <alberto.soutullo639@gmail.com>
2024-03-27 16:36:46 +0100

    Removed other wrongly added function hehe

--- src/utils/path.py
+++ src/utils/path.py
@@ -1,8 +1,6 @@
 # Python Imports
-import os
 import logging
 from pathlib import Path
-from typing import List
 from result import Result, Err, Ok
 
 
@@ -21,13 +19,3 @@ def prepare_path(file_location: str) -> Result[Path, str]:
         return Err(f'Error creating {file_location_path.parent}. {e}')
 
     return Ok(file_location_path)
-
-
-def get_files_from_folder_path(path: Path) -> Result[List, str]:
-    if path.exists():
-        files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
-        logger.info(f"Found {len(files)} in {path}")
-        logger.debug(f"Files are: {files}")
-        return Ok(files)
-
-    return Err(f"{path} does not exist.")

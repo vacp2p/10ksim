c5ce9dbf435a230bcc04dcfb1efac6701f64f9ac
Alberto Soutullo <alberto.soutullo639@gmail.com>
2024-05-13 16:42:24 +0200

    Msg tracker - parsing (#24)
    * WIP: Parsing and creating df from log files done
    
    * First tracking version structure
    
    * Add PR suggestions

---
+++ src/mesh_analysis/tracers/message_tracer.py
@@ -0,0 +1,20 @@
+# Python Imports
+import pandas as pd
+from abc import ABC, abstractmethod
+from typing import List, Optional
+
+# Project Imports
+
+
+class MessageTracer(ABC):
+
+    def __init__(self):
+        self._patterns: Optional[List] = None
+
+    @property
+    def patterns(self) -> List:
+        return self._patterns
+
+    @abstractmethod
+    def trace(self, parsed_logs: List) -> pd.DataFrame:
+        pass

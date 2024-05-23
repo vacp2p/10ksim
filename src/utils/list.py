3abd05c06f38379017906da135958f864c2e0498
Alberto Soutullo <alberto.soutullo639@gmail.com>
2024-05-23 15:48:14 +0200

    Added midstrap for ordering

--- src/utils/list.py
+++ src/utils/list.py
@@ -10,15 +10,19 @@ def order_by_groups(list_to_order: List) -> List:
 
     nodes = []
     bootstrap = []
+    midstrap = []
     others = []
     for item in list_to_order:
         if item.startswith('nodes'):
             nodes.append(item)
         elif item.startswith('bootstrap'):
             bootstrap.append(item)
+        elif item.startswith('midstrap'):
+            midstrap.append(item)
         else:
             others.append(item)
     nodes.sort(key=get_default_format_id)
     bootstrap.sort(key=get_default_format_id)
+    midstrap.sort(key=get_default_format_id)
 
-    return list(chain(others, bootstrap, nodes))
+    return list(chain(others, bootstrap, midstrap, nodes))

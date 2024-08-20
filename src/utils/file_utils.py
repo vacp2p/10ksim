36bf9b2569f303453ecc2a33e8a13938b9c09b8d
Alberto Soutullo <alberto.soutullo639@gmail.com>
2024-08-20 11:31:35 +0200

    Fix get_files_from_folder_path when using '*'

--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -20,12 +20,13 @@ def read_yaml_file(file_path: str) -> Dict:
     return data
 
 
-def get_files_from_folder_path(path: Path, extension: str = '*') -> Result[List, str]:
+def get_files_from_folder_path(path: Path, extension: str = '*') -> Result[List[str], str]:
     if not path.exists():
         return Err(f"{path} does not exist.")
 
-    if extension[0] != '.':
-        extension = '.' + extension
+    if not extension.startswith('*') and not extension.startswith('.'):
+        extension = '*.' + extension
+
     files = [p.name for p in path.glob(extension) if p.is_file()]
     logger.debug(f"Found {len(files)} files in {path}")
     logger.debug(f"Files are: {files}")

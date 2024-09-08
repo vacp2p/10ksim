ae5142f1192dbe10ae464f69cb08944dbdd3dc62
Alberto Soutullo <alberto.soutullo639@gmail.com>
2024-09-08 19:57:20 +0200

    Message store check (#39)
    * Add container for store message retrieval
    
    * Add store analysis example
    
    * Add method to check store messages
    
    * Fix bug when using cursor

--- example_log_analysis.py
+++ example_log_analysis.py
@@ -7,11 +7,12 @@ from src.mesh_analysis.waku_message_log_analyzer import WakuMessageLogAnalyzer
 
 if __name__ == '__main__':
     # Timestamp of the simulation
-    timestamp = "[2024-08-05T15:45:00, 2024-08-05T15:49:00]"
+    timestamp = "[2024-08-14T11:11:00, 2024-08-14T12:05:00]"
     # Example of data analysis from cluster
-    # log_analyzer = WakuMessageLogAnalyzer(timestamp, dump_analysis_dir='test_logs_victoria')
+    log_analyzer = WakuMessageLogAnalyzer(timestamp, dump_analysis_dir='store')
     # Example of data analysis from local
-    log_analyzer = WakuMessageLogAnalyzer(local_folder_to_analyze='data', dump_analysis_dir='test_logs_victoria')
+    # log_analyzer = WakuMessageLogAnalyzer(local_folder_to_analyze='lpt_duptest_debug', dump_analysis_dir='lpt_duptest_debug/notion')
 
-    log_analyzer.analyze_message_logs()
+    log_analyzer.analyze_message_logs(True)
+    log_analyzer.check_store_messages()
     log_analyzer.analyze_message_timestamps(time_difference_threshold=2)

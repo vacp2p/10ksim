e8620faeedcc8f9c36b8e507f86349969288966d
Benjamin Arntzen <zorlin@gmail.com>
2024-09-16 00:47:59 -0400

    Finished manifest work for heterogenous deploy, working on deployment scripts now

--- deployment/deployment.sh
+++ deployment/deployment.sh
@@ -1,10 +1,10 @@
+# Spawn bootstrap nodes
 kubectl apply -f bootstrap.yaml
+# Wait to see that they're healthy
 kubectl rollout status --watch --timeout=30000s statefulset/bootstrap -n zerotesting
-#kubectl apply -f midstrap.yaml
-#kubectl rollout status --watch --timeout=30000s statefulset/midstrap -n zerotesting
+# Spawn all the nodes
 kubectl apply -f nodes-nwaku.yaml
 kubectl apply -f nodes-gowaku.yaml
 echo "We have deployed all nodes, please watch Prometheus or Grafana to see when they have reached a healthy state."
-echo "Please note you cannot (yet) rely on the Ready state as it does not actually indicate an unhealthy peer, just one that is not ready for bootstrapping from."
-#kubectl rollout status --watch --timeout=30000s statefulset/nodes -n zerotesting
+kubectl rollout status --watch --timeout=30000s statefulset/nodes -n zerotesting
 kubectl apply -f publisher.yaml

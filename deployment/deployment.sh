6984ae546fa555a136aacf42621c33365bd79f49
Benjamin Arntzen <zorlin@gmail.com>
2024-09-16 07:07:10 -0400

    new script, new bootstrap

--- deployment/deployment.sh
+++ deployment/deployment.sh
@@ -7,4 +7,21 @@ kubectl apply -f nodes-nwaku.yaml
 kubectl apply -f nodes-gowaku.yaml
 echo "We have deployed all nodes, please watch Prometheus or Grafana to see when they have reached a healthy state."
 kubectl rollout status --watch --timeout=30000s statefulset/nodes -n zerotesting
+echo "We believe everything has rolled out. Deploying publisher now."
 kubectl apply -f publisher.yaml
+echo "We have deployed the publisher, please watch Grafana to see if it's working."
+# 2100 seconds or 35 minutes
+for i in {0..209}
+do
+  timeelapsed=$(10*(i+1))
+  echo "Sleeping for 10 seconds, $timeelapsed/2100 seconds"
+  sleep 10
+done
+# Tear down publisher
+kubectl delete -f publisher.yaml
+# Wait 60 seconds for publisher to despawn
+# Tear down nodes
+kubectl delete -f nodes-nwaku.yaml
+kubectl delete -f nodes-gowaku.yaml
+# Tear down bootstrap
+kubectl delete -f bootstrap.yaml

7f9530150952733c672e454ba33cf16e935d39e6
Benjamin Arntzen <zorlin@gmail.com>
2024-09-22 21:35:24 -0400

    Various fixes

--- deployment/deployment-preview.sh
+++ deployment/deployment-preview.sh
@@ -1,10 +1,35 @@
+# Timestamp
+echo "Started at $(date)"
+# Spawn bootstrap nodes
 kubectl apply -f bootstrap.yaml
+# Wait to see that they're healthy
 kubectl rollout status --watch --timeout=30000s statefulset/bootstrap -n zerotesting
-#kubectl apply -f midstrap.yaml
-#kubectl rollout status --watch --timeout=30000s statefulset/midstrap -n zerotesting
+# Spawn all the nodes
 kubectl apply -f nodes-nwaku.yaml
-kubectl apply -f nodes-gowaku.yaml
+kubectl apply -f nodes-gowaku-preview.yaml
 echo "We have deployed all nodes, please watch Prometheus or Grafana to see when they have reached a healthy state."
-echo "Please note you cannot (yet) rely on the Ready state as it does not actually indicate an unhealthy peer, just one that is not ready for bootstrapping from."
-#kubectl rollout status --watch --timeout=30000s statefulset/nodes -n zerotesting
+kubectl rollout status --watch --timeout=30000s statefulset/nodes -n zerotesting
+echo "We believe everything has rolled out. Deploying publisher now."
 kubectl apply -f publisher.yaml
+# Timestamp
+echo "Deploying publisher at $(date)"
+echo "We have deployed the publisher, please watch Grafana to see if it's working."
+# 2100 seconds or 35 minutes
+for i in {0..209}
+do
+  timeelapsed=$(10*(i+1))
+  echo "Sleeping for 10 seconds, $timeelapsed/2100 seconds"
+  sleep 10
+done
+# Tear down publisher
+# Timestamp
+echo "We'll stop publishing. Stopped publisher at $(date)"
+
+kubectl delete -f publisher.yaml
+# Wait 60 seconds for publisher to despawn
+echo "Now we'll tear down the whole cluster. Stopped cluster at $(date)"
+# Tear down nodes
+kubectl delete -f nodes-nwaku.yaml
+kubectl delete -f nodes-gowaku.yaml
+# Tear down bootstrap
+kubectl delete -f bootstrap.yaml

dc54a4227b75c187a73c0387a217d6932831fc4e
Alberto Soutullo <alberto.soutullo639@gmail.com>
2024-12-18 12:11:45 +0100

    Use number of shards and number of nodes as a class internal data

--- src/mesh_analysis/waku_message_log_analyzer.py
+++ src/mesh_analysis/waku_message_log_analyzer.py
@@ -18,8 +18,10 @@ logger = logging.getLogger(__name__)
 
 
 class WakuMessageLogAnalyzer:
-    def __init__(self, timestamp_to_analyze: str = None, dump_analysis_dir: str = None,
+    def __init__(self, num_shards: int, timestamp_to_analyze: str = None, dump_analysis_dir: str = None,
                  local_folder_to_analyze: str = None):
+        self._num_shards = num_shards
+        self._num_nodes = None
         self._validate_analysis_location(timestamp_to_analyze, local_folder_to_analyze)
         self._set_up_paths(dump_analysis_dir, local_folder_to_analyze)
         self._timestamp = timestamp_to_analyze
@@ -38,14 +40,15 @@ class WakuMessageLogAnalyzer:
             logger.error(result.err_value)
             exit(1)
 
-    def _get_victoria_config_parallel(self, pod_name: str) -> Dict:
+    def _get_victoria_config_parallel(self, node_index: int, num_nodes: int, num_shards: int) -> Dict:
+        shard = int(node_index // (num_nodes / num_shards))
         return {"url": "https://vmselect.riff.cc/select/logsql/query",
                 "headers": {"Content-Type": "application/json"},
                 "params": [
                     {
-                        "query": f"kubernetes_container_name:waku AND kubernetes_pod_name:{pod_name} AND received relay message AND _time:{self._timestamp}"},
+                        "query": f"kubernetes_container_name:waku AND kubernetes_pod_name:nodes-{shard}-{node_index} AND received relay message AND _time:{self._timestamp}"},
                     {
-                        "query": f"kubernetes_container_name:waku AND kubernetes_pod_name:{pod_name} AND sent relay message AND _time:{self._timestamp}"}]
+                        "query": f"kubernetes_container_name:waku AND kubernetes_pod_name:nodes-{shard}-{node_index} AND sent relay message AND _time:{self._timestamp}"}]
                 }
 
     def _get_victoria_config_single(self) -> Dict:
@@ -142,18 +145,18 @@ class WakuMessageLogAnalyzer:
         waku_tracer.with_received_pattern()
         waku_tracer.with_sent_pattern()
 
-        config = victoria_config_func(node_index)
+        config = victoria_config_func(node_index, self._num_nodes, self._num_shards)
         reader = VictoriaReader(config, waku_tracer)
         data = reader.read()
         logger.debug(f'Nodes-{node_index} analyzed')
 
         return data
 
-    def _read_logs_concurrently(self, n_nodes: int) -> List[pd.DataFrame]:
+    def _read_logs_concurrently(self) -> List[pd.DataFrame]:
         dfs = []
         with ProcessPoolExecutor() as executor:
             futures = {executor.submit(self._read_logs_for_node, i, self._get_victoria_config_parallel): i
-                       for i in range(n_nodes)}
+                       for i in range(self._num_nodes)}
 
             for i, future in enumerate(as_completed(futures)):
                 i = i + 1
@@ -161,15 +164,15 @@ class WakuMessageLogAnalyzer:
                     df = future.result()
                     dfs.append(df)
                     if i % 50 == 0:
-                        logger.info(f'Processed {i}/{n_nodes} nodes')
+                        logger.info(f'Processed {i}/{self._num_nodes} nodes')
 
                 except Exception as e:
                     logger.error(f'Error retrieving logs for node {futures[future]}: {e}')
 
         return dfs
 
-    def _has_issues_in_cluster_parallel(self, n_nodes: int) -> bool:
-        dfs = self._read_logs_concurrently(n_nodes)
+    def _has_issues_in_cluster_parallel(self) -> bool:
+        dfs = self._read_logs_concurrently()
         dfs = self._merge_dfs(dfs)
 
         self._message_hashes = dfs[0].index.get_level_values(0).unique().tolist()
@@ -233,10 +236,9 @@ class WakuMessageLogAnalyzer:
     def analyze_message_logs(self, parallel=False):
         if self._timestamp is not None:
             logger.info('Analyzing from server')
-            n_nodes = self._get_number_nodes()
-            logger.info(f'Detected {n_nodes} pods')
-            has_issues = self._has_issues_in_cluster_parallel(
-                n_nodes) if parallel else self._has_issues_in_cluster_single()
+            self._num_nodes = self._get_number_nodes()
+            logger.info(f'Detected {self._num_nodes} pods')
+            has_issues = self._has_issues_in_cluster_parallel() if parallel else self._has_issues_in_cluster_single()
             if has_issues:
                 match file_utils.get_files_from_folder_path(Path(self._dump_analysis_dir), extension="csv"):
                     case Ok(data_files_names):

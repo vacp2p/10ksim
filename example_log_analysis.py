39058a9f0090394d9ebe3f734c2ea663e7800afd
Alberto Soutullo <alberto.soutullo639@gmail.com>
2024-12-26 17:58:27 +0100

    Update example file with the newest changes

--- example_log_analysis.py
+++ example_log_analysis.py
@@ -7,9 +7,10 @@ from src.mesh_analysis.waku_message_log_analyzer import WakuMessageLogAnalyzer
 
 if __name__ == '__main__':
     # Timestamp of the simulation
-    timestamp = "[2024-08-14T11:11:00, 2024-08-14T12:05:00]"
+    timestamp = "[2024-12-26T16:01:00, 2024-12-26T16:20:00]"
+    stateful_sets = ["fserver", "lpserver", "store"]
     # Example of data analysis from cluster
-    log_analyzer = WakuMessageLogAnalyzer(2, timestamp, dump_analysis_dir='local_data/shard_tests/')
+    log_analyzer = WakuMessageLogAnalyzer(stateful_sets, timestamp, dump_analysis_dir='local_data/mixed_enviroment/')
     # Example of data analysis from local
     # log_analyzer = WakuMessageLogAnalyzer(local_folder_to_analyze='lpt_duptest_debug', dump_analysis_dir='lpt_duptest_debug/notion')
 

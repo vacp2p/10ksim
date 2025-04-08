00c5ee66ec34ff8480ee6dbc064e127be1128e5f
Alberto Soutullo <alberto.soutullo639@gmail.com>
2025-04-08 21:00:22 +0200

    Add plot function and check for the message jump

--- src/mesh_analysis/waku_message_log_analyzer.py
+++ src/mesh_analysis/waku_message_log_analyzer.py
@@ -345,6 +345,64 @@ class WakuMessageLogAnalyzer:
             for jump in time_jumps:
                 logger.info(f'{file}: {jump[0]} to {jump[1]} -> {jump[2]}')
 
+    def plot_message_distribution_mixnet(self, received_summary_path: Path, sent_summary_path: Path, plot_title: str, dump_path: Path) -> Result[None, str]:
+        if not received_summary_path.exists():
+            error = f"Received summary file {received_summary_path} does not exist"
+            logger.error(error)
+            return Err(error)
+
+        if not sent_summary_path.exists():
+            error = f"Sent summary file {sent_summary_path} does not exist"
+            logger.error(error)
+            return Err(error)
+
+        sns.set_theme()
+
+        df_received = pd.read_csv(received_summary_path, parse_dates=["timestamp"])
+        df_received.set_index(['shard', 'msg_hash', 'timestamp'], inplace=True)
+
+        df_sent = pd.read_csv(sent_summary_path, parse_dates=["timestamp"])
+        df_sent.set_index(['shard', 'msg_hash', 'timestamp'], inplace=True)
+
+        ###################
+        for (shard, msg_hash), group in df_sent.groupby(['shard', 'msg_hash']):
+            received_group = df_received.loc[shard, msg_hash]
+
+            for i in range(0, len(group) - 1, 2):
+                receiver_id = group.iloc[i]['receiver_peer_id']
+                sender_id = group.iloc[i + 1]['sender_peer_id']
+
+                # Check if the receiver_peer_id in the current row matches the sender_peer_id in the next row
+                if receiver_id != sender_id:
+                    logger.error(f"Mismatch detected at shard {shard}, msg_hash {msg_hash}, timestamp {group.index[i]}: "
+                          f"{receiver_id} != {sender_id}")
+
+            # Check that the last receiver in the received group matches the last sender in the sent data
+            if received_group.iloc[-1]['receiver_peer_id'] != group.iloc[-1]['receiver_peer_id']:
+                logger.error(f"Final mismatch at shard {shard}, msg_hash {msg_hash}: "
+                      f"{received_group.iloc[-1]['receiver_peer_id']} != {group.iloc[-1]['sender_peer_id']}")
+        ###################
+
+        latest_received = df_received.groupby(level='msg_hash').apply(
+            lambda x: x.index.get_level_values('timestamp').max()
+        ).rename("last_received")
+
+        sent_times = df_sent.groupby("msg_hash").apply(lambda x: x.index.get_level_values('timestamp').min()).rename(
+            "injected_at")
+
+        merged = pd.concat([sent_times, latest_received], axis=1, join="inner")
+        merged["time_to_reach"] = (merged["last_received"] - merged["injected_at"]).dt.total_seconds()
+
+        plt.figure(figsize=(12, 6))
+        ax = sns.boxplot(x="time_to_reach", data=merged.reset_index(), color="skyblue")
+
+        plt.xlabel('Time to Reach Node (seconds)')
+        plt.title(plot_title)
+        plt.savefig(dump_path)
+        plt.show()
+
+        return Ok(None)
+
 
     def plot_message_distribution(self, received_summary_path: Path, plot_title: str, dump_path: Path) -> Result[None, str]:
         """

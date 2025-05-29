f081b8838fe056d87bd127862cfa1346504c0a8b
Alberto Soutullo <alberto.soutullo639@gmail.com>
2025-05-29 15:41:11 +0200

    WIP: Add builder to base stack analysis class

--- src/mesh_analysis/stacks/stack_analysis.py
+++ src/mesh_analysis/stacks/stack_analysis.py
@@ -1,11 +1,21 @@
+# Python Imports
 from abc import ABC, abstractmethod
+
+# Project Imports
+from src.mesh_analysis.readers.builders.victoria_reader_builder import VictoriaReaderBuilder
+
 
 
 class StackAnalysis(ABC):
 
-    def __init__(self, **kwargs):
+    def __init__(self, reader_builder: VictoriaReaderBuilder, **kwargs):
+        self._reader_builder = reader_builder
         self._kwargs = kwargs
 
     @abstractmethod
-    def get_reliability_data(self, n_jobs: int, **kwargs):
+    def get_node_logs(self, n_jobs: int, **kwargs):
+        pass
+
+    @abstractmethod
+    def dump_node_logs(self,  n_jobs: int, **kwargs):
         pass

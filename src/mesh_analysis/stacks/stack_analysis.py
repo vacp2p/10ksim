88accf16deccb970fbb32549f2ccc93a28f016a4
Alberto Soutullo <alberto.soutullo639@gmail.com>
2025-06-04 19:05:21 +0200

    WIP: Rename functions in stack_analysis.py

--- src/mesh_analysis/stacks/stack_analysis.py
+++ src/mesh_analysis/stacks/stack_analysis.py
@@ -1,5 +1,7 @@
 # Python Imports
 from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import List
 
 # Project Imports
 from src.mesh_analysis.readers.builders.victoria_reader_builder import VictoriaReaderBuilder
@@ -13,9 +15,9 @@ class StackAnalysis(ABC):
         self._kwargs = kwargs
 
     @abstractmethod
-    def get_node_logs(self, n_jobs: int, **kwargs):
+    def get_all_node_dataframes(self, n_jobs: int):
         pass
 
     @abstractmethod
-    def dump_node_logs(self,  n_jobs: int, **kwargs):
+    def dump_node_logs(self,  n_jobs: int, identifiers: List[str], dump_path: Path):
         pass

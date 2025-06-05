b32596644e53b0c433857c7d0101b9a7fdb0c5fe
Alberto Soutullo <alberto.soutullo639@gmail.com>
2025-06-05 14:33:45 +0200

    Expand example_log_analysis.py

--- example_log_analysis.py
+++ example_log_analysis.py
@@ -3,9 +3,10 @@ from pathlib import Path
 
 # Project Imports
 import src.logger.logger
+from src.mesh_analysis.analyzers.nimlibp2p_analyzer import Nimlibp2pAnalyzer
 from src.mesh_analysis.analyzers.waku import waku_plots
 from src.mesh_analysis.analyzers.waku.waku_analyzer import WakuAnalyzer
-from src.mesh_analysis.analyzers.waku.waku_plots import plot_message_distribution
+
 
 
 if __name__ == '__main__':
@@ -19,10 +20,9 @@ if __name__ == '__main__':
              'container_name': 'waku',
              'extra_fields': ['kubernetes.pod_name', 'kubernetes.pod_node_name']
              }
-    log_analyzer = WakuAnalyzer(dump_analysis_dir='local_data/simulations_data/refactor/',
+    log_analyzer = WakuAnalyzer(dump_analysis_dir='local_data/simulations_data/refactor_nimlibp2p/',
                                 # local_folder_to_analyze='local_data/simulations_data/waku_simu3/log/',
                                 **stack)
-
     log_analyzer.analyze_reliability(n_jobs=4)
     log_analyzer.check_store_messages()
     log_analyzer.check_filter_messages()
@@ -30,4 +30,20 @@ if __name__ == '__main__':
     waku_plots.plot_message_distribution(Path('local_data/simulations_data/refactor/summary/received.csv'),
                                          'Title',
                                          Path('local_data/simulations_data/refactor/plot.png'))
-
+    stack = {'type': 'vaclab',
+             'url': 'https://vmselect.riff.cc/select/logsql/query',
+             'start_time': '2025-06-04T17:10:00',
+             'end_time': '2025-06-04T17:30:00',
+             'reader': 'victoria',
+             'stateful_sets': ['pod'],
+             'nodes_per_statefulset': [10],
+             'container_name': 'container-0',
+             'extra_fields': ['kubernetes.pod_name', 'kubernetes.pod_node_name']
+             }
+    log_analyzer = Nimlibp2pAnalyzer(dump_analysis_dir='local_data/simulations_data/refactor_nimlibp2p/',
+                                # local_folder_to_analyze='local_data/simulations_data/waku_simu3/log/',
+                                **stack)
+    log_analyzer.analyze_reliability(n_jobs=4)
+    waku_plots.plot_message_distribution_libp2pmix(Path('local_data/simulations_data/refactor_nimlibp2p/summary/received.csv'),
+                                         'Title',
+                                         Path('local_data/simulations_data/refactor_nimlibp2p/plot.png'))

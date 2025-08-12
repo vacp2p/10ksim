cf0e1711fb2a51c1129b3c74fff88900c7ac3f24
Alberto Soutullo <alberto.soutullo639@gmail.com>
2025-08-12 14:13:16 +0200

    Update build script version

--- deployment-utilities/docker_utilities/waku/get_enr/build.sh
+++ deployment-utilities/docker_utilities/waku/get_enr/build.sh
@@ -1,2 +1,2 @@
-docker build -t <your-registry>/getenr:v1.0.0 .
-docker push <your-registry>/getenr:v1.0.0
+docker build -t <your-registry>/getenr:v1.1.0 .
+docker push <your-registry>/getenr:v1.1.0

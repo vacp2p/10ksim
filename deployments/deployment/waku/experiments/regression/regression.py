4d7d9ea1e3a0c01642365a1acdbaa6edf905b2fb
PearsonWhite <PearsonWhite602@gmail.com>
2025-09-11 13:22:07 -0400

    Make _metadata_event use dict events (#131)
    * Make _metadata_event use dict events
    
    * Looks for events by comparing dicts
    
    * Separate out parsing logic for metadata event
    
    * Make class method _get_metadata_event
    
    * typing

--- deployments/deployment/waku/experiments/regression/regression.py
+++ deployments/deployment/waku/experiments/regression/regression.py
@@ -4,9 +4,10 @@ import os
 import time
 from argparse import Namespace
 from contextlib import ExitStack
+from copy import deepcopy
 from datetime import datetime, timedelta
 from pathlib import Path
-from typing import Any, Optional
+from typing import Any, Callable, Dict, List, Optional, Tuple
 
 from kubernetes.client import ApiClient
 from pydantic import BaseModel, ConfigDict, Field
@@ -16,7 +17,11 @@ from deployment.base_experiment import BaseExperiment
 from deployment.builders import build_deployment
 from kube_utils import (
     assert_equals,
+    dict_apply,
+    dict_get,
+    dict_partial_compare,
     dict_set,
+    dict_visit,
     get_cleanup,
     get_flag_value,
     kubectl_apply,
@@ -25,6 +30,80 @@ from kube_utils import (
 from registry import experiment
 
 logger = logging.getLogger(__name__)
+
+
+def parse_events_log(
+    log_path: str,
+    events_list: List[Tuple[Dict[str, str], str]],
+    *,
+    extract: Callable[[dict], Any] | None = None,
+) -> dict:
+    """
+    Return a new dict constructed by parsing the event log.
+    Each line in `log_path` is converted to an event (dict).
+    If the event contains all of the (key, value) items from a dict in `events_list`, then the event is converted to a new value using `extract(event)` and added at `path` in the new dict, where `path` is the value from the `events_list`.
+
+    :param log_path: Path to events log.
+    :param events_list: List of tuples mapping a dict to compare to the line to a path in the return dict.
+    :param extract: A function mapping the json loaded from the event log to the value in the return dict.
+    :return: dict constructed from extracting matchign lines from log_path and converting them to values using `extract`.
+    :rtype: dict
+    """
+    if extract is None:
+        extract = lambda event: datetime.strptime(event["timestamp"], "%Y-%m-%d %H:%M:%S")
+    return_dict = {}
+    with open(log_path, "r") as events_log:
+        for line in events_log:
+            event = json.loads(line)
+            for key, path in events_list:
+                try:
+                    if dict_partial_compare(event, key):
+                        new_value = extract(event)
+                        dict_set(
+                            return_dict,
+                            path,
+                            new_value,
+                            sep=".",
+                        )
+                except KeyError:
+                    pass
+    return return_dict
+
+
+def format_metadata_timestamps(metadata: dict) -> dict:
+    def format_item(node):
+        try:
+            return node.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
+        except AttributeError:
+            pass
+
+    return dict_apply(metadata, format_item)
+
+
+def get_valid_shifted_times(deltatime_map: dict[str, timedelta], metadata: dict) -> dict:
+    shifted = deepcopy(metadata)
+    for path, delta in deltatime_map.items():
+        time_value = dict_get(shifted, path, default=None, sep=".")
+        if time_value is not None:
+            shifted_time = time_value + delta
+            dict_set(shifted, path, shifted_time, sep=".", replace_leaf=True)
+
+    filtered = {}
+
+    def filter(path, obj):
+        try:
+            start_dt = obj["start"]
+            end_dt = obj["end"]
+            if end_dt <= start_dt:
+                return
+            dict_set(filtered, path / "start", start_dt)
+            dict_set(filtered, path / "end", end_dt)
+        except (KeyError, TypeError) as e:
+            pass
+
+    dict_visit(shifted, filter)
+
+    return filtered
 
 
 @experiment(name="waku-regression-nodes")
@@ -61,56 +140,48 @@ class WakuRegressionNodes(BaseExperiment, BaseModel):
             event = {"event": event}
         return super()._preprocess_event(event)
 
-    def _metadata_event(self):
-        metadata = {}
-        events_dict = {
-            "wait_for_clear_finished": ("complete.start", timedelta(seconds=0)),
-            "internal_run_finished": ("complete.end", timedelta(seconds=30)),
-            "publisher_deploy_start": ("stable.start", timedelta(minutes=3)),
-            "publisher_messages_finished": ("stable.end", timedelta(seconds=-30)),
-        }
-        with open(self.events_log_path, "r") as events:
-            for line in events:
-                event = json.loads(line)
-                for key, (path, offset) in events_dict.items():
-                    try:
-                        dt = datetime.strptime(event["timestamp"], "%Y-%m-%d %H:%M:%S")
-                        if event["event"] == key:
-                            dict_set(
-                                metadata,
-                                path,
-                                (dt, offset),
-                                sep=".",
-                            )
-                    except KeyError:
-                        pass
+    @classmethod
+    def _get_metadata_event(_cls, events_log_path: str):
+        events_list = [
+            ({"event": "wait_for_clear_finished"}, ("complete.start", timedelta(seconds=0))),
+            ({"event": "internal_run_finished"}, ("complete.end", timedelta(seconds=30))),
+            ({"event": "publisher_deploy_start"}, ("stable.start", timedelta(minutes=3))),
+            (
+                {"event": "deployment", "service": "waku/publisher", "phase": "start"},
+                ("stable.start", timedelta(minutes=3)),
+            ),
+            ({"event": "publisher_messages_finished"}, ("stable.end", timedelta(seconds=-30))),
+        ]
 
-        # Apply time offsets unless the interval would not make sense if we did.
-        for interval_type, interval in metadata.items():
-            start_dt = interval["start"][0] + interval["start"][1]
-            end_dt = interval["end"][0] + interval["end"][1]
-            if end_dt <= start_dt:
-                start_dt = interval["start"][0]
-                end_dt = interval["end"][0]
-            start_formatted = start_dt.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
-            interval["start"] = start_formatted
-            end_formatted = end_dt.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
-            interval["end"] = end_formatted
+        # Strip the timedelta for the conversion, to get a list of Tuple[match_dict : dict, path : str].
+        events_maps = [(obj[0], obj[1][0]) for obj in events_list]
+        metadata = parse_events_log(events_log_path, events_maps)
+
+        # Get timedeltas for each path. dict of {path : timedelta}.
+        deltatime_map = {obj[1][0]: obj[1][1] for obj in events_list}
+        shifted = get_valid_shifted_times(deltatime_map, metadata)
+        metadata.update(shifted)
+
+        metadata = format_metadata_timestamps(metadata)
 
         # Add links.
-        links = {
+        links_map = {
             "grafana": "https://grafana.vaclab.org/d/jIrqsZTIz/nwaku?orgId=1&from={start}&to={end}&timezone=utc",
             "victoria": "https://vlselect.vaclab.org/select/vmui/?#/?query=*&g0.start_input={start}&g0.end_input={end}&g0.relative_time=none",
         }
+        # For interval_type in [completed, stable] (if they were added).
         for interval_type in metadata.keys():
             try:
-                for link_type, base in links.items():
+                for link_type, base in links_map.items():
                     metadata[interval_type][link_type] = base.format(
                         start=metadata[interval_type]["start"], end=metadata[interval_type]["end"]
                     )
             except KeyError:
                 pass
-        self.log_event(metadata)
+        return metadata
+
+    def _metadata_event(self, events_log_path: str):
+        self.log_event(self.__class__._get_metadata_event(self.events_log_path))
 
     def _run(
         self,
@@ -126,6 +197,8 @@ class WakuRegressionNodes(BaseExperiment, BaseModel):
         logger.info("Building kubernetes configs.")
         nodes = self._build(workdir, values_yaml, "nodes")
         bootstrap = self._build(workdir, values_yaml, "bootstrap")
+
+        self.log_event({"event": "deployment", "service": "waku/publisher", "phase": "start"})
         publisher = self._build(workdir, values_yaml, "publisher")
 
         # Sanity check
@@ -217,4 +290,4 @@ class WakuRegressionNodes(BaseExperiment, BaseModel):
 
         logger.info("Finished waku regression test.")
         self.log_event("internal_run_finished")
-        self._metadata_event()
+        self._metadata_event(self.events_log_path)

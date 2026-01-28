6e8c306ec2d0ecdf3305bbd5eaf063b8248ecf2d
Pearson White <PearsonWhite602@gmail.com>
2026-01-28 18:48:34 -0500

    Add Waku functionality for pod_api_requester

---
+++ deployments/pod_api_requester/waku.py
@@ -0,0 +1,36 @@
+import logging
+from typing import Union
+
+from pydantic import NonNegativeInt
+
+from pod_api_requester.configs import Target
+from pod_api_requester.pod_api_requester import (
+    _DEFAULTS,
+    pod_api_request,
+    wrap_arg,
+)
+
+logger = logging.getLogger(__name__)
+
+
+async def waku_publish(
+    namespace: str,
+    target: Union[Target, str],
+    content_topic: str = "/my-app/1/dst/proto",
+    cluster_id: NonNegativeInt = 2,
+    port: NonNegativeInt = 8645,
+    msg_size_kbytes: NonNegativeInt = 10,
+) -> dict:
+    return await pod_api_request(
+        namespace=namespace,
+        service_name=_DEFAULTS["service_name"],
+        app=_DEFAULTS["app"],
+        url_template="http://{target_ip}:{node_port}/waku/relay",
+        data={
+            "target": wrap_arg(target),
+            "content_topic": content_topic,
+            "cluster_id": cluster_id,
+            "port": port,
+            "msg_size_kbytes": msg_size_kbytes,
+        },
+    )

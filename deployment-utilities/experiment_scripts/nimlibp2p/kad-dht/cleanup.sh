11d024e43f66296344d91d7752920ba2c9300b82
radiken <lizhuolun199904@gmail.com>
2026-04-29 11:52:20 +0100

    chore: isolate framework additions by removing KAD DHT experiment files

--- deployment-utilities/experiment_scripts/nimlibp2p/kad-dht/cleanup.sh
+++
@@ -1,5 +0,0 @@
-NAMESPACE="nimlibp2p"
-KUBECONFIG=~/.kube/config
-kubectl --kubeconfig $KUBECONFIG --namespace $NAMESPACE delete -f bootstrap.yaml
-kubectl --kubeconfig $KUBECONFIG --namespace $NAMESPACE delete -f nodes.yaml
-kubectl --kubeconfig $KUBECONFIG --namespace $NAMESPACE delete -f probe.yaml

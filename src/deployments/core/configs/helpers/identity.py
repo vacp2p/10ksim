60cc0599b62ffd601ad1c3541172c076f6c0e2f6
Pearson White <PearsonWhite602@gmail.com>
2026-06-11 16:51:38 -0400

    fix imports

--- src/deployments/core/configs/helpers/identity.py
+++ src/deployments/core/configs/helpers/identity.py
@@ -3,21 +3,32 @@ from typing import Optional
 
 # Project Imports
 from src.deployments.core.configs.helpers.utils import HigherConfigTypes
-from src.deployments.core.configs.pod import PodTemplateSpecConfig
+from src.deployments.core.configs.pod import PodConfig, PodTemplateSpecConfig
 from src.deployments.core.configs.statefulset import StatefulSetConfig, StatefulSetSpecConfig
 
 
-def apply_pod_template_spec_config(config: PodTemplateSpecConfig, namespace: str, app: str):
-    config.name = "pod"
+def apply_pod_config(config: PodConfig, namespace: str, name: str, app: str):
+    config.name = name
     config.namespace = namespace
     if app:
         config.with_app(app)
 
 
-def apply_stateful_set_spec_config(config: StatefulSetSpecConfig, namespace: str, app: str):
+def apply_pod_template_spec_config(
+    config: PodTemplateSpecConfig, namespace: str, name: str, app: str
+):
+    config.name = name
+    config.namespace = namespace
     if app:
         config.with_app(app)
-    apply_pod_template_spec_config(config.pod_template_spec_config, namespace, app)
+
+
+def apply_stateful_set_spec_config(
+    config: StatefulSetSpecConfig, namespace: str, name: str, app: str
+):
+    if app:
+        config.with_app(app)
+    apply_pod_template_spec_config(config.pod_template_spec_config, namespace, name, app)
 
 
 def apply_stateful_set_config(
@@ -25,15 +36,17 @@ def apply_stateful_set_config(
 ):
     config.name = name
     config.namespace = namespace
-    apply_stateful_set_config(config.stateful_set_spec, namespace, app)
+    apply_stateful_set_spec_config(config.stateful_set_spec, namespace, name, app)
 
 
 def apply_identity(config: HigherConfigTypes, name: str, namespace: str, app: Optional[str] = None):
     if isinstance(config, StatefulSetConfig):
-        return apply_stateful_set_config(config, namespace, name, app)
+        return apply_stateful_set_config(config, namespace=namespace, name=name, app=app)
     if isinstance(config, StatefulSetSpecConfig):
-        return apply_stateful_set_spec_config(config, namespace, app)
+        return apply_stateful_set_spec_config(config, namespace=namespace, name=name, app=app)
+    if isinstance(config, PodConfig):
+        return apply_pod_config(config, namespace=namespace, name=name, app=app)
     elif isinstance(config, PodTemplateSpecConfig):
-        return apply_pod_template_spec_config(config, namespace, app)
+        return apply_pod_template_spec_config(config, namespace=namespace, app=app)
 
     raise ValueError(f"Unknown config type: {type(config)}")

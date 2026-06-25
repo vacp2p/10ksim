b75d9bee6017e4001a6ce8859f2925bdc8434210
Alberto Soutullo <albertorendo@status.im>
2026-06-25 16:59:06 +0200

    Update str with Paths

--- src/deployments/core/base_bridge.py
+++ src/deployments/core/base_bridge.py
@@ -43,7 +43,7 @@ PROJ_ROOT = Path(__file__).parent.parent.parent
 
 
 def find_events(
-    log_path: Union[str, Path],
+    log_path: Path,
     key: Dict[str, str],
 ) -> Iterator[dict]:
     """
@@ -53,7 +53,7 @@ def find_events(
     then the event is converted to a new value using `extract(event)`
     """
     results = []
-    with Path(log_path).open("r") as events_log:
+    with log_path.open("r") as events_log:
         for line in events_log:
             event = json.loads(line)
             if dict_partial_compare(event, key):
@@ -62,8 +62,8 @@ def find_events(
 
 
 def parse_events_log(
-    log_path: str,
-    events_list: List[Tuple[Dict[str, str], Union[str, Path]]],
+    log_path: Path,
+    events_list: List[Tuple[Dict[str, str], Path]],
     *,
     extract: Callable[[dict], Any] | None = None,
 ) -> dict:
@@ -83,7 +83,7 @@ def parse_events_log(
     if extract is None:
         extract = lambda event: datetime.strptime(event["timestamp"], "%Y-%m-%d %H:%M:%S")
     return_dict = {}
-    with open(log_path, "r") as events_log:
+    with log_path.open("r") as events_log:
         for line in events_log:
             event = json.loads(line)
             for key, path in events_list:
@@ -175,7 +175,7 @@ class BaseBridge(BaseModel):
     nodes_key: str = "nodes_per_statefulset"
 
     def _get_metadata_from_events_list(
-        self, events_log_path: str, events_list: List[EventMapping]
+        self, events_log_path: Path, events_list: List[EventMapping]
     ) -> dict:
         """Extract events in from a given event log."""
         # Strip the timedelta for the conversion, to get a list of Tuple[match_dict : dict, path : str].

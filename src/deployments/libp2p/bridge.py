905a2527d0f5400a247bfa2c93939e1e6c500a3e
Alberto Soutullo <albertorendo@status.im>
2026-06-26 16:05:24 +0200

    Update libp2p bridge

--- src/deployments/libp2p/bridge.py
+++ src/deployments/libp2p/bridge.py
@@ -1,46 +1,37 @@
-# Python imports
+# Python Imports
 import logging
 from datetime import timedelta
-from pathlib import Path
-from typing import Literal
+from typing import Dict, Literal
 
 # Project Imports
-from src.deployments.core.base_bridge import BaseBridge, EventMapping
+from src.deployments.core.event_window_bridge import (
+    EventWindowBridge,
+    EventWindowEndpoint,
+    event_window,
+)
 from src.deployments.libp2p.builders.helpers import LIBP2P_CONTAINER_NAME
 
 logger = logging.getLogger(__name__)
 
 
-class Bridge(BaseBridge):
+class Bridge(EventWindowBridge):
     interval: Literal["complete", "stable"] = "complete"
     """Time interval for start and end times.
 
     complete: From the beginning to the end of the whole experiment run code
 
     stable: Just the time where nodes should have stablized.
-    Calculated in _get_metadata_events by adding an offset from when message publishing starts and ends.
+    Calculated by adding offsets to message publishing start/end events.
     """
 
-    def get_metadata(self, events_log_path: str) -> dict:
-        metadata = super().get_metadata(events_log_path)
-        events = self._get_metadata_events(events_log_path)
-        metadata["results"] = events
-        metadata["stack"]["start_time"] = events[self.interval]["start"]
-        metadata["stack"]["end_time"] = events[self.interval]["end"]
-        metadata["stack"]["container_name"] = LIBP2P_CONTAINER_NAME
-        return metadata
-
-    def _get_metadata_events(self, events_log_path: str):
-        events = [
-            ("wait_for_clear_finished", Path("complete", "start"), timedelta(seconds=0)),
-            ("internal_run_finished", Path("complete", "end"), timedelta(seconds=30)),
-            ("start_messages", Path("stable", "start"), timedelta(minutes=3)),
-            ("publisher_messages_finished", Path("stable", "end"), timedelta(seconds=-30)),
-        ]
-        events_list = list(
-            map(
-                lambda obj: EventMapping(key={"event": obj[0]}, target=obj[1], time_shift=obj[2]),
-                events,
-            )
-        )
-        return self._get_metadata_from_events_list(events_log_path, events_list)
+    container_name: str = LIBP2P_CONTAINER_NAME
+    event_windows: Dict[str, Dict[str, EventWindowEndpoint]] = {
+        "complete": {
+            "start": event_window("wait_for_clear_finished"),
+            "end": event_window("internal_run_finished", timedelta(seconds=30)),
+        },
+        "stable": {
+            "start": event_window("start_messages", timedelta(minutes=3)),
+            "end": event_window("publisher_messages_finished", timedelta(seconds=-30)),
+        },
+    }

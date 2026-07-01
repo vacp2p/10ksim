4db3c5939878dbdede6981ec4a7c062aeaad833e
Pearson White <PearsonWhite602@gmail.com>
2026-07-01 15:28:45 -0400

    address review comments

--- src/deployments/utils/flatten.py
+++ src/deployments/utils/flatten.py
@@ -2,7 +2,7 @@ def flatten(obj):
     if isinstance(obj, dict):
         for value in obj.values():
             yield from flatten(value)
-    elif isinstance(obj, list):
+    elif isinstance(obj, (list, tuple, set)):
         for value in obj:
             yield from flatten(value)
     else:

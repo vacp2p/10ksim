708e302b21b195fa5b1226f067451110776ca750
Alberto Soutullo <albertorendo@status.im>
2026-07-22 15:28:58 +0200

    Add ServiceDiscoveryBridge

--- src/deployments/libp2p/service_discovery_bridge.py
+++ src/deployments/libp2p/service_discovery_bridge.py
@@ -1,26 +1,23 @@
-import logging
-from typing import Dict, Literal
+from typing import List, Literal
 
-from src.deployments.core.event_window_bridge import (
-    EventWindowBridge,
-    EventWindowEndpoint,
-    event_window,
-)
+import src.deployments.core.event_window_bridge as event_window_bridge
 from src.deployments.libp2p.builders.helpers import LIBP2P_CONTAINER_NAME
 
-logger = logging.getLogger(__name__)
 
-
-class ServiceDiscoveryBridge(EventWindowBridge):
+class ServiceDiscoveryBridge(event_window_bridge.EventWindowBridge):
     interval: Literal["complete", "discovery"] = "complete"
     container_name: str = LIBP2P_CONTAINER_NAME
-    event_windows: Dict[str, Dict[str, EventWindowEndpoint]] = {
-        "complete": {
-            "start": event_window("wait_for_clear_finished"),
-            "end": event_window("service_discovery_finished"),
-        },
-        "discovery": {
-            "start": event_window("service_discovery_started"),
-            "end": event_window("service_discovery_finished"),
-        },
-    }
+
+    def event_windows(self) -> List[event_window_bridge.EventWindow]:
+        return [
+            event_window_bridge.EventWindow(
+                key="complete",
+                start=event_window_bridge.EventBound("wait_for_clear_finished"),
+                end=event_window_bridge.EventBound("service_discovery_finished"),
+            ),
+            event_window_bridge.EventWindow(
+                key="discovery",
+                start=event_window_bridge.EventBound("service_discovery_started"),
+                end=event_window_bridge.EventBound("service_discovery_finished"),
+            ),
+        ]

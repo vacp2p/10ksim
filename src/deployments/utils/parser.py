88f90bb621498a22369fad4a6cc2cd432a0ec0e8
Pearson White <PearsonWhite602@gmail.com>
2026-07-23 18:18:15 -0400

    Change auto generation of experiment config CLI params to allow passing in complex types.
    For types that inherit from pydantic.BaseModel, allow JSON str input.
    --image='{"repo":"pearsonwhite/nwaku","tag":"quic-b778d16"}'
    
    For types that have a from_str static method, use that if JSON parsing fails.
    --image="pearsonwhite/nwaku:quic-b778d16"
    
    Falls back on direct constructor with input str as the argument.

--- src/deployments/utils/parser.py
+++ src/deployments/utils/parser.py
@@ -1,7 +1,8 @@
 # Python Imports
+import argparse
 from typing import Any, Literal, Union, get_args, get_origin
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 from pydantic.fields import FieldInfo
 
 ARG_NOT_SET = object()
@@ -33,6 +34,45 @@ def _unwrap_optional(annotation):
     return annotation
 
 
+def get_from_str(annotation: Any, field_name: str):
+    def from_str(input_str: str) -> Any:
+        # Convert using model_validate_json.
+        errors = []
+        try:
+            if issubclass(annotation, BaseModel):
+                return annotation.model_validate_json(input_str)
+        except (ValidationError, ValueError) as e:
+            errors.append(
+                f"Failed convert string with model_validate_json. Field: `{field_name}` Error: `{e}`"
+            )
+
+        # Convert using custom from_str method.
+        try:
+            return annotation.from_str(input_str)
+        except AttributeError:
+            pass
+        except (ValidationError, ValueError, TypeError) as e:
+            err = argparse.ArgumentTypeError(
+                f"Failed to convert string with from_str. Field: `{field_name}` Error: `{e}`"
+            )
+            errors.append(err)
+
+        # Convert using direct instantiation.
+        try:
+            return annotation(input_str)
+        except (ValidationError, ValueError, TypeError) as e:
+            err = argparse.ArgumentTypeError(
+                f"Failed to convert string directly. Field: `{field_name}` Error: `{e}`"
+            )
+            errors.append(err)
+
+        if len(errors) == 1:
+            raise errors[0]
+        raise ValueError(f"Failed to convert field from str. Errors: {errors}")
+
+    return from_str
+
+
 def _field_to_arg(field_name: str, field: FieldInfo) -> tuple[str, dict[str, Any]]:
     annotation = _unwrap_optional(field.annotation)
     flag = f"--{field_name.replace('_', '-')}"
@@ -41,12 +81,11 @@ def _field_to_arg(field_name: str, field: FieldInfo) -> tuple[str, dict[str, Any
         "dest": field_name,
         "default": ARG_NOT_SET,
         "required": False,
-        "type": str,
     }
 
     if annotation is bool:
         kwargs["action"] = "store_true"
-        del kwargs["type"]
+        # For bool, "type" should not be set.
 
     if annotation in (int, float, str):
         kwargs["type"] = annotation
@@ -62,6 +101,10 @@ def _field_to_arg(field_name: str, field: FieldInfo) -> tuple[str, dict[str, Any
         if choices:
             kwargs["type"] = type(choices[0])
             kwargs["choices"] = choices
+
+    if "type" not in kwargs.keys() and annotation is not bool:
+        from_str = get_from_str(annotation, field_name)
+        kwargs["type"] = from_str
 
     if field.description:
         kwargs["help"] = field.description
